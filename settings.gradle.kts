3da11d6991b3c2dd6fed0e87ae99329e9326b602
Yaksh Patel <bitu4patel@gmail.com>
2023-11-06 19:54:18 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practical11_5b6_22012022013"
+include(":app")
+ 
